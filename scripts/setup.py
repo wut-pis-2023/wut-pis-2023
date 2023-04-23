5eab35bbba9101a924022d5ce0f1333eb2c76f5d
bedkowski.patrick <bedkowskisite@gmail.com>
2023-04-23 18:05:14 +0200

    Changed filename of the upload package

--- scripts/setup.py
+++ scripts/setup.py
@@ -16,6 +16,7 @@ setup(
     install_requires=requirements,
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
     ],
 )
